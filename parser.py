5aaabfe1eb442b675cb98387e2da55ece051dd5d
Neeraj Kumar <neerajkumar@Neerajs-iMac.local>
2024-02-08 18:56:28 +0530

    Updated requirements and modified code

--- parser.py
+++ parser.py
@@ -4,21 +4,18 @@
 # This script defines the Document and Page classes that parse_table.py uses
 # to streamline the flow of information through the script.
 
-import os
-import math
-import shutil
-import logging
-import jenkspy
 import itertools
+import logging
+import os
 import statistics
-import numpy as np
-import pandas as pd
 from io import BytesIO
 
 import cv2
-import pytesseract
-from PIL import Image, ImageOps
 import fitz
+import jenkspy
+import numpy as np
+import pandas as pd
+import pytesseract
 
 # Initialize logger
 logger = logging.getLogger('parse_table')
@@ -26,7 +23,6 @@ logger.setLevel(logging.DEBUG)
 
 
 class Document:
-
 
     def __init__(self, doc_name, doc_dir, output_dir):
 
@@ -42,9 +38,8 @@ class Document:
         # Make folder for this document in /output_dir to hold output files
         os.mkdir(os.path.join(self.output_dir, self.doc_name))
 
-
     def split_pages(self):
-        '''
+        """
         1. Splits the input pdf into pages
         2. Writes a temporary image for each page to a byte buffer
         3. Loads the image as a numpy array using cv2.imread()
@@ -53,7 +48,7 @@ class Document:
         Notes:
         PyMuPDF's getPixmap() has a default output of 96dpi, while the desired
         resolution is 300dpi, hence the zoom factor of 300/96 = 3.125 ~ 3.
-        '''
+        """
 
         logger.debug("Splitting PDF into pages")
 
@@ -61,12 +56,12 @@ class Document:
         zoom_factor = 3
         for i in range(len(doc)):
             # Load page and get pixmap
-            page = doc.loadPage(i)
-            pixmap = page.getPixmap(matrix=fitz.Matrix(zoom_factor, zoom_factor))
+            page = doc.load_page(i)
+            pixmap = page.get_pixmap(matrix=fitz.Matrix(zoom_factor, zoom_factor))
 
             # Initialize bytes buffer and write PNG image to buffer
             buffer = BytesIO()
-            buffer.write(pixmap.getPNGData())
+            buffer.write(pixmap.tobytes())
             buffer.seek(0)
 
             # Load image from buffer as array, append to self.pages, close buffer
@@ -77,11 +72,10 @@ class Document:
 
         return None
 
-
     def export_data(self):
-        '''
+        """
         Exports the fully-shaped data in self.doc_data to output_dir
-        '''
+        """
 
         output_name = self.doc_name + ".csv"
         logger.debug(f"Writing table to file: {output_name}")
@@ -91,7 +85,6 @@ class Document:
         ), index=False)
 
         return self
-
 
     def parse_doc(self):
 
@@ -108,6 +101,8 @@ class Document:
                 # Get headers if first page, else use existing headers
                 if not idx:
                     page = Page(i, idx + 1, self.doc_name, self.output_dir)
+                    if not page.check_for_table():
+                        continue
                     page.parse_page()
                     self.headers = page.get_headers()
                 elif idx and not self.headers:
@@ -115,10 +110,13 @@ class Document:
                 else:
                     page = Page(i, idx + 1, self.doc_name, self.output_dir,
                                 headers=self.headers)
+                    if not page.check_for_table():
+                        continue
                     page.parse_page()
 
                 # Append dataframe of parsed data to self.page_dfs
                 self.page_dfs.append(page.get_data())
+
             except Exception as e:
                 logger.info(f"    ERROR IN {self.doc_name}, page {idx + 1}: {str(e)}")
 
@@ -143,7 +141,6 @@ class Document:
 
 class Page:
 
-
     def __init__(self, img, page_num, doc_name, output_dir, headers=None):
         self.img = img
         self.page_num = page_num
@@ -157,14 +154,13 @@ class Page:
         self.contours = []
         self.text_data = None
 
-
     def preprocess_image(self):
-        '''
+        """
         1. Converts input color image to grayscale
         2. Applies thresholding to self.img_gray
         3. Inverts the image
         4. Detects skew and auto-deskews self.img_gray
-        '''
+        """
 
         # Convert color image to grayscale
         img = cv2.cvtColor(self.img, cv2.COLOR_RGB2GRAY)
@@ -176,11 +172,11 @@ class Page:
         img = 255 - img
 
         # Deskew image
-        img_rot = cv2.dilate(img, np.ones((1,3)), iterations=10)
-        img_rot = cv2.erode(img, np.ones((1,3)), iterations=50)
+        # img_rot = cv2.dilate(img, np.ones((1, 3)), iterations=10)
+        img_rot = cv2.erode(img, np.ones((1, 3)), iterations=50)
         cnt, _ = cv2.findContours(img_rot, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         angle_list = [x[-1] for x in list(map(cv2.minAreaRect, cnt)) \
-            if abs(x[-1]) != 0 and abs(x[-1]) != 90]
+                      if abs(x[-1]) != 0 and abs(x[-1]) != 90]
 
         try:
             angle = statistics.median(angle_list)
@@ -194,9 +190,9 @@ class Page:
 
         (h, w) = img.shape[:2]
         center = (w // 2, h // 2)
-        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
         img = cv2.warpAffine(img,
-                             M,
+                             matrix,
                              (w, h),
                              flags=cv2.INTER_CUBIC,
                              borderMode=cv2.BORDER_REPLICATE)
@@ -206,17 +202,16 @@ class Page:
 
         return self
 
-
     def detect_table(self):
-        '''
+        """
         1. Finds horizontal and vertical kernels in self.img_gray
         2. Combines them to get boxes
-        '''
+        """
 
         logger.debug("    Detecting lines on page")
 
         # Define kernels
-        kernel_length = np.array(self.img_gray).shape[1]//80
+        kernel_length = np.array(self.img_gray).shape[1] // 80
         h_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
         v_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
         sq_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
@@ -237,13 +232,12 @@ class Page:
 
         return self
 
-
     def get_contours(self):
-        '''
+        """
         1. Detects contours in the binary image stored in self.table
         2. Filters detected contours for quality (filters out lines, etc.)
         3. Appends final contours to self.contours
-        '''
+        """
 
         # Detect all contours
         contours, _ = cv2.findContours(self.table,
@@ -253,18 +247,38 @@ class Page:
         # Filter out non-box contours
         for c in contours:
             x, y, w, h = cv2.boundingRect(c)
-            if  w > 20 and h > 20 and w < (0.5 * self.img.shape[1]):
+            if w > 20 and h > 20 and w < (0.5 * self.img.shape[1]):
                 self.contours.append(c)
 
         logger.debug(f"        {len(self.contours)} boxes detected")
 
         return self
 
+    def check_for_table(self):
+        """
+        Checks if a table exists on the page.
+
+        Returns:
+        - True if a table exists, False otherwise.
+        """
+
+        # Preprocess image
+        self.preprocess_image()
+
+        # Detect table
+        self.detect_table()
+        self.get_contours()
+
+        # If contours are found, consider it as a table
+        if len(self.contours) > 0:
+            return True
+        else:
+            return False
 
     def draw_contours(self):
-        '''
+        """
         Draws the detected contours onto the original image and exports it.
-        '''
+        """
 
         # Overlay contours on color image
         img_temp = cv2.cvtColor(self.img_gray, cv2.COLOR_GRAY2BGR)
@@ -278,37 +292,42 @@ class Page:
 
         return self
 
-
     @staticmethod
     def read_text_from_coords(img, coords):
-        '''
+        """
         Takes the image in question, finds the bounding box given by the given
         coordinates, and returns text in the box.
-        '''
+        """
         x, y, w, h = coords
 
         # Crop image, then add 20px white border
-        img_to_read = img[y:y+h, x:x+w]
-        img_to_read = np.pad(img_to_read, pad_width=20, mode='constant', constant_values=0)
+        img_to_read = img[y:y + h, x:x + w]
+        # img_to_read = np.pad(img_to_read, pad_width=20, mode='constant', constant_values=0)
 
         # Read text
-        text = pytesseract.image_to_string(img_to_read)
-
+        text = pytesseract.image_to_string(img_to_read, config='--psm 6')
+        print(text, "text")
         return text
 
-
     def read_cells(self):
-        '''
+        """
         1. Constructs a dataframe from self.contours
-        2. Extracts coordinates of bounding rectangle for each contour
+        2. Extract coordinates of bounding rectangle for each contour
         3. Reads text within the given coordinates in self.img_gray
-        '''
+        """
 
         logger.debug("    Reading detected cells in table")
 
         # Get dataframe and coordinates of bounding boxes
-        df = pd.DataFrame(self.contours).rename({0: 'contours'}, axis=1)
-        df['xywh'] = df['contours'].apply(cv2.boundingRect)
+        # df = pd.DataFrame(self.contours).rename({0: 'contours'}, axis=1)
+
+        contours_array = [np.array(c, dtype=np.int32) for c in self.contours]
+
+        # Calculate bounding rectangles for each contour
+        bounding_rects = [cv2.boundingRect(contour) for contour in contours_array]
+
+        # Create DataFrame with bounding rectangles and contours
+        df = pd.DataFrame({'contours': contours_array, 'xywh': bounding_rects})
 
         # Read cells
         self.img_gray = 255 - self.img_gray
@@ -322,13 +341,12 @@ class Page:
 
         return self
 
-
     @staticmethod
     def calculate_dimensions(df, headers):
-        '''
+        """
         Given a dataframe with (x, y, w, h) data, uses large gaps in coordinates
         to estimate the number of rows and columns in the data.
-        '''
+        """
 
         # Calculate number of rows
         num_rows = 1
@@ -336,7 +354,7 @@ class Page:
         for i in range(len(df)):
             if i == 0:
                 continue
-            elif df.iloc[i]['y'] > (df.iloc[i-1]['y'] + 0.8 * df.iloc[i-1]['h']):
+            elif df.iloc[i]['y'] > (df.iloc[i - 1]['y'] + 0.8 * df.iloc[i - 1]['h']):
                 num_rows += 1
 
         if headers:
@@ -348,14 +366,13 @@ class Page:
             for i in range(len(df)):
                 if i == 0:
                     continue
-                elif df.iloc[i]['x'] > (df.iloc[i-1]['x'] + 0.8 * df.iloc[i-1]['w']):
+                elif df.iloc[i]['x'] > (df.iloc[i - 1]['x'] + 0.8 * df.iloc[i - 1]['w']):
                     num_cols += 1
 
         return num_rows, num_cols
 
-
     def reconstruct_table(self):
-        '''
+        """
         1. Calculates number of rows, cols
         2. Use Jenks optimization to assign row and col numbers
         3. If necessary, adjusts column assignment if number of rows, cols
@@ -363,7 +380,7 @@ class Page:
         4. If there are merged cells, fill merged cells with 0 to complete table
         5. Pivor table into final format
         6. Export results to csv
-        '''
+        """
 
         logger.debug("    Parsing data into table...")
 
@@ -378,12 +395,12 @@ class Page:
 
         # Use Jenks optimization to get natural breaks in y-coordinates, then
         # assign row number based on natural breaks
-        row_breaks = jenkspy.jenks_breaks(df['y'], nb_class=num_rows)
+        row_breaks = jenkspy.jenks_breaks(df['y'], num_rows)
         calculate_row_num = lambda y: sum(list(map(lambda x: 1 if x < y else 0, row_breaks[1:])))
         df['row_num'] = df['y'].apply(calculate_row_num)
 
         # Repeat for columns
-        col_breaks = jenkspy.jenks_breaks(df['x'], nb_class=num_cols)
+        col_breaks = jenkspy.jenks_breaks(df['x'], num_cols)
         calculate_col_num = lambda y: sum(list(map(lambda x: 1 if x < y else 0, col_breaks[1:])))
         df['col_num'] = df['x'].apply(calculate_col_num)
 
@@ -393,7 +410,7 @@ class Page:
             while num_rows * num_cols < len(df):
                 num_cols += 1
             logger.debug(f"        Adjusting estimate to {num_rows} rows and {num_cols} columns.")
-            col_breaks = jenkspy.jenks_breaks(df['x'], nb_class=num_cols)
+            col_breaks = jenkspy.jenks_breaks(df['x'], num_cols)
             calculate_col_num = lambda y: sum(list(map(lambda x: 1 if x < y else 0, col_breaks[1:])))
             df['col_num'] = df['x'].apply(calculate_col_num)
 
@@ -402,8 +419,8 @@ class Page:
             df = df \
                 .set_index(['row_num', 'col_num']) \
                 .reindex(pd.MultiIndex.from_tuples(
-                    set(itertools.product(df['row_num'], df['col_num'])))
-                ) \
+                set(itertools.product(df['row_num'], df['col_num'])))
+            ) \
                 .reset_index() \
                 .rename({'level_0': 'row_num', 'level_1': 'col_num'}, axis=1) \
                 .sort_values(by=['row_num', 'col_num']) \
@@ -434,11 +451,10 @@ class Page:
 
         return self
 
-
     def export_data(self):
-        '''
+        """
         Exports the fully-shaped data in self.text_data to output_dir
-        '''
+        """
 
         output_name = self.doc_name + "_page" + str(self.page_num) + ".csv"
         logger.debug(f"    Writing table to file: {output_name}")
@@ -449,14 +465,11 @@ class Page:
 
         return self
 
-
     def get_headers(self):
         return self.headers
 
-
     def get_data(self):
         return self.text_data
-
 
     def parse_page(self):
 
@@ -466,6 +479,6 @@ class Page:
             .draw_contours() \
             .read_cells() \
             .reconstruct_table()
-            # .export_data()
+        # .export_data()
 
         return None
